3e6fce63821f30d4bb725709440fc05024d5e9a9
Paul Theby <paultheby@gmail.com>
2024-03-11 18:22:21 -0500

    Sign-Up Component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'sign_up', loadComponent: () => import('./features/auth/sign-up/sign-up.component').then((c)=>
+  c.SignUpComponent)
+},
+
+];
+
+
+
