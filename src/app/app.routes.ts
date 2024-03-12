e4c54fec06a9a36bf3927b4c7768ce38d7b3f5b0
Paul Theby <paultheby@gmail.com>
2024-03-11 19:07:39 -0500

    sign-up form reactive form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   {
-    path: 'sign_up', loadComponent: () => import('./features/auth/sign-up/sign-up.component').then((c)=>
-  c.SignUpComponent)
-},
-
+    path: 'sign-up',
+    loadComponent: () =>
+      import('./features/auth/sign-up/sign-up.component').then(
+        (c) => c.SignUpComponent
+      ),
+  },
 ];
-
-
-
--- src/app/features/auth/sign-up/sign-up.component.ts
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
+  signupForm = new FormGroup({
+    user: new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+      password_confirmation: new FormControl(''),
+    }),
+    agent: new FormGroup({
+      first_name: new FormControl(''),
+      last_name: new FormControl(''),
+      npn: new FormControl(''),
+      state: new FormControl(''),
+      city: new FormControl(''),
+    }),
+  });
 
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
 }
