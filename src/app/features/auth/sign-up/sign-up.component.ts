96fac76a45b6725f71b88d5efc18b62717ea9e44
Paul Theby <paultheby@gmail.com>
2024-03-11 20:15:17 -0500

    1

--- src/app/features/auth/sign-up/sign-up.component.ts
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -1,30 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
+isLoading = false
+
+
+
   signupForm = new FormGroup({
     user: new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(''),
-      password_confirmation: new FormControl(''),
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl(['',Validators.required,
+      Validators.minLength(6)]),
+      password_confirmation: new FormControl('',Validators.required),
     }),
     agent: new FormGroup({
-      first_name: new FormControl(''),
-      last_name: new FormControl(''),
-      npn: new FormControl(''),
-      state: new FormControl(''),
-      city: new FormControl(''),
+      first_name: new FormControl('',Validators.required),
+      last_name: new FormControl('',Validators.required),
+      npn: new FormControl('',Validators.required),
+      state: new FormControl('',Validators.required),
+      city: new FormControl('',Validators.required),
     }),
   });
 
-  onSubmit() {
+  onSignup() {
     console.log(this.signupForm.value);
   }
+  
 }
