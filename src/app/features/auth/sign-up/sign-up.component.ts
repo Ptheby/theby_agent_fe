497f3c739f1129a221a6a777f5eae808f2372afb
Paul Theby <paultheby@gmail.com>
2024-03-17 16:22:08 -0500

    trying to get form to submit

--- src/app/features/auth/sign-up/sign-up.component.ts
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -1,40 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+standalone: true,
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css',
+  styleUrls: ['./sign-up.component.css'],
+  imports: [ReactiveFormsModule,CommonModule],
 })
 export class SignUpComponent {
-isLoading = false
-}
+  isLoading = false;
+  signupForm: FormGroup;
 
 
-//   signupForm = new FormGroup({
-//     user: new FormGroup({
-//       email: new FormControl('',[Validators.required,Validators.email]),
-//       password: new FormControl(['',Validators.required,
-//       Validators.minLength(6)]),
-//       password_confirmation: new FormControl('',Validators.required),
-//     }),
-//     agent: new FormGroup({
-//       first_name: new FormControl('',Validators.required),
-//       last_name: new FormControl('',Validators.required),
-//       npn: new FormControl('',Validators.required),
-//       state: new FormControl('',Validators.required),
-//       city: new FormControl('',Validators.required),
-//     }),
-//   });
 
-//   onSignup() {
-//     console.log(this.signupForm.value);
-//   }
-//   // onLogin() {
-//   //   this.router.navigate(['./auth/login'])
-//   // }
-// }
+  constructor () {
+    this.signupForm = new FormGroup ({
+      user: new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        password_confirmation: new FormControl('', Validators.required),
+      }),
+      agent: new FormGroup({
+        first_name: new FormControl('', Validators.required),
+        last_name: new FormControl('', Validators.required),
+        npn: new FormControl('', Validators.required),
+        state: new FormControl('', Validators.required),
+      }),
+    });
+  }
+  onSignup(){
+    console.log(this.signupForm.value)
+      }
+
+  }
+
+
