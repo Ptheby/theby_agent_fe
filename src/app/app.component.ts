49f1aca7fdabfdffca6e469842fb89e2d6afc7bc
Paul Theby <paultheby@gmail.com>
2024-03-24 00:13:12 -0500

    basics of NavBar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthComponent } from './features/auth/auth/auth.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HttpClientModule,AuthComponent],
+  imports: [RouterOutlet,HttpClientModule,AuthComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
