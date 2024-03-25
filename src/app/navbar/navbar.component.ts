aba40c917de7946fdd4d0609efc2d53ec80512f2
Paul Theby <paultheby@gmail.com>
2024-03-24 19:47:04 -0500

    environments changed apiURL to onrender

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../features/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,5 +10,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-onLogout(){}
+onLogout(){};
+  constructor(private authService: AuthService) {
+this.onLogout= this.authService.logout
+  }
+
 }
