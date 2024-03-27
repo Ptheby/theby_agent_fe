63e83d17462da97bb166ec6be6ed7926f3e6ce78
Paul Theby <paultheby@gmail.com>
2024-03-26 21:48:32 -0500

    dynamic apiUrl
    attempting to use a dynamic api Url based on environment so that i can signup in production in both frontend and backend

--- src/app/features/auth/auth.service.ts
+++ src/app/features/auth/auth.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+
   private readonly tokenSubject = new BehaviorSubject<string | null>(null);
 
   constructor(private http: HttpClient, private router: Router) {}
 
   login(email: string, password: string) {
-    return this.http.post<{ token: string }>('http://localhost:3000/login', {
+    const apiUrl =environment.apiUrl;
+    return this.http.post<{ token: string }>('apiUrl', {
       email,
       password,
     });
@@ -37,5 +40,6 @@ export class AuthService {
 
 
   signUp(user: any) {
-    return this.http.post('http://localhost:3000/users/create_with_agent', user)
+    const apiUrl =environment.apiUrl;
+    return this.http.post('apiUrl/users/create_with_agent', user)
   }}
