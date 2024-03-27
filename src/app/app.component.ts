409a4250fff0ee39a99272f34ce76185bab7f754
Paul Theby <paultheby@gmail.com>
2024-03-26 21:39:41 -0500

    Update app.component.ts
    trying to determine if my production enviroment is running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthComponent } from './features/auth/auth/auth.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,4 +13,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 })
 export class AppComponent {
   title = 'theby_agent_fe';
+  constructor() {
+    if (environment.production) {
+      console.log('Production environment');
+    } else {
+      console.log('Non-production environment');
+    }
+  }
 }
+
+
