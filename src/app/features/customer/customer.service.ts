f9952fa88b39dfc9437f26ba80421a7e12154eb1
Paul Theby <paultheby@gmail.com>
2024-03-29 20:32:28 -0500

    Actions in CustomerService.ts
    getCustomerById and Get all Customers. Created View Customers Component to create the component to view all customers so that I can then selected one to view.

--- src/app/features/customer/customer.service.ts
+++ src/app/features/customer/customer.service.ts
@@ -3,23 +3,35 @@ import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Customer } from '../../shared/models/customer';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerService {
-apiUrl =environment.apiUrl;
+  apiUrl = environment.apiUrl;
 
+  onAddCustomer() {}
+  customers = [];
+  constructor(private http: HttpClient) {}
 
-
-  onAddCustomer(){};
-  customers=[];
-  constructor(private http: HttpClient) { }
-
+  // add a customer with an observable returning a post method to my environmental api URL plus the path for the create action including address
   addCustomer(customerData: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl+"/customers/create_with_address",customerData);
+    return this.http.post<any>(
+      this.apiUrl + '/customers/create_with_address',
+      customerData
+    );
 
+    //same thing as the add but getting a getting a customer with a get method based on id
+  }
+  getCustomerById(id: any): Observable<any> {
+    const url = `${this.apiUrl}/customers/${id}`;
+    return this.http.get<any>(url);
+  }
+  getAllCustomers(): Observable<any> {
+    return this.http.get<any>(
+      this.apiUrl + '/customers')
 
+  }
 
-
-}}
+}
