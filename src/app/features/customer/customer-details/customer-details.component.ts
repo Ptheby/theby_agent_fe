00b2646ee7f2f4da66faf40a882c3c95d7b29b76
Paul Theby <paultheby@gmail.com>
2024-04-02 19:10:15 -0500

    displaying correct customer details in customer details component

--- src/app/features/customer/customer-details/customer-details.component.ts
+++ src/app/features/customer/customer-details/customer-details.component.ts
@@ -4,27 +4,33 @@ import { CustomerService } from '../customer.service';
 
 import { ActivatedRoute, Router } from '@angular/router';
 import { Customer } from '../customer.model';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-customer-details',
   standalone: true,
   imports: [],
   templateUrl: './customer-details.component.html',
-  styleUrl: './customer-details.component.css'
+  styleUrl: './customer-details.component.css',
 })
 export class CustomerDetailsComponent implements OnInit {
-selectedCustomer:Customer|undefined;
+  selectedCustomer: Customer | undefined;
 
-
-
-
-  constructor(private customerService: CustomerService, private route: ActivatedRoute, private router:Router) {}
+  constructor(
+    private customerService: CustomerService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       const customerId = +params['id']; // Extract customer ID from route parameters
-      this.customerService.getCustomerById(customerId).subscribe((data: Customer) => {
-        this.selectedCustomer = data; // Set the customer details
-      });
+      console.log('CustomerID:', customerId);
+      this.customerService
+        .getCustomerById(customerId)
+        .subscribe((data: Customer) => {
+          this.selectedCustomer = data;
+          console.log(this.selectedCustomer.first_name)
+          // Set the customer details
+        });
     });
   }
 }
-
--- src/app/features/customer/customer.service.ts
+++ src/app/features/customer/customer.service.ts
@@ -1,46 +1,36 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, throwError } from 'rxjs';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Customer } from './customer.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CustomerService implements OnInit {
+export class CustomerService {
   apiUrl = environment.apiUrl;
 
-  customers: Customer[] = [];
   constructor(private http: HttpClient) {}
-  ngOnInit(): void {
-    this.fetchCustomers();
-  }
-  // add a customer with an observable returning a post method to my environmental api URL plus the path for the create action including address
+
   addCustomer(customerData: any): Observable<any> {
     return this.http.post<any>(
-      this.apiUrl + '/customers/create_with_address',
+      `${this.apiUrl}/customers/create_with_address`,
       customerData
     );
-
-    //same thing as the add but getting a getting a customer with a get method based on id
-  }
-  getCustomerById(id: any): Observable<any> {
-    const url = `${this.apiUrl}/customers/${id}`;
-    return this.http.get<any>(url);
-  }
-  getAllCustomers(): Observable<any> {
-    return this.fetchCustomers(); // Call fetchCustomers and return its result
   }
 
-  private fetchCustomers(): Observable<Customer[]> {
-    return this.http.get<Customer[]>(this.apiUrl + '/customers').pipe(
+  getCustomerById(id: any): Observable<Customer> {
+    return this.http.get<Customer>(`${this.apiUrl}/customers/${id}`);
+  }
+
+  getAllCustomers(): Observable<Customer[]> {
+    return this.http.get<any>(`${this.apiUrl}/customers`).pipe(
       map((response: any) => {
-        const customers: Customer[] = response.customers; // Extract the customers array from the response
+        const customers: Customer[] = response.customers;
         return customers;
       }),
       catchError((error) => {
-        // Handle errors if any
         console.error('Error fetching customers:', error);
         return throwError(error);
       })
