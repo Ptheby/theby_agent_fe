5fdeb11be89bdd9856d2efb39e5090913dfc2619
Paul Theby <paultheby@gmail.com>
2024-04-05 22:49:52 -0500

    Create address component and Create Assign agent Component

---
+++ src/app/agent/agent-assignment/agent-assignment.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AgentService } from '../agent.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-agent-assignment',
+  standalone: true,
+  imports: [],
+  templateUrl: './agent-assignment.component.html',
+  styleUrl: './agent-assignment.component.css'
+})
+export class AgentAssignmentComponent {
+
+
+
+  constructor(
+    private agentService: AgentService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+}
