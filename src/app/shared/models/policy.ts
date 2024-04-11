f82849dc7114e7e9602da6f5697907a91d94ebcc
Paul Theby <paultheby@gmail.com>
2024-04-11 18:46:14 -0500

    corrected customer-detail
    changed spelling of some items i changed to match backend

--- src/app/shared/models/policy.ts
+++ src/app/shared/models/policy.ts
@@ -4,9 +4,9 @@ export class Policy {
   constructor(
   public  policy_type: string,
   public  exp_date: Date,
-  public   term_legnth: string,
-  public  customerId: number,
-  public   agentId: number,
+  public   term_length: string,
+  public  customer_id: number,
+  public   agent_id: number,
   public   insurance_company_id: number,
   public   createdAt: Date,
   public   updatedAt: Date,
